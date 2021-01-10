337f65b357cb92721b6ab0423cb3413b84aba170
Nataliia Pyvovartseva <raxma@ukr.net>
2021-01-11 00:19:21 +0200

    ADD Router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import reducers from './reducers' ;
+import reducers from './reducers';
 import middleware from './middleware';
 import './index.css';
 import App from './components/App';
 
 const store = createStore(reducers, middleware);
+const history = createBrowserHistory();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={history}>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
