3c39c5cf6a257058858a8337aea716bb0846304f
Nataliia Pyvovartseva <raxma@ukr.net>
2021-02-19 11:56:42 +0200

    Add Custom Theme. Add Routing. Add LoadingBar

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Route, Switch } from 'react-router-dom'
 import Loading from 'react-redux-loading-bar';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { Grid } from '@material-ui/core';
 import { handelInitialData } from '../actions/shared';
-import Home from './Home';
 
+import Nav from './Nav';
+import Home from './Home';
+import NewQuestion from './NewQuestion';
+import Question from './Question';
+
+
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#263238',
+    },
+    secondary: {
+      main: '#263238',
+    }
+  },
+});
 
 class App extends Component {
 
@@ -13,14 +32,35 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <Loading />
-        {this.props.authedUser ?
-          null :
-          <Home />
-        }
+      <ThemeProvider theme={theme}>
+        <div>
+          <Loading />
+          <Nav />
+          {this.props.authedUser ?
+            null :
+            <Grid container>
+              <Grid item xs sm></Grid>
+              <Grid item xs={10} sm={4}>
 
-      </div>
+                <Switch>
+                  <Route exact path='/'>
+                    <Home />
+                  </Route>
+                  <Route path='/leaders'>
+                    <Question />
+                  </Route>
+                  <Route path='/new'>
+                    <NewQuestion />
+                  </Route>
+                </Switch>
+
+              </Grid>
+              <Grid item xs sm></Grid>
+            </Grid>
+          }
+
+        </div>
+      </ThemeProvider>
     )
   }
 }
