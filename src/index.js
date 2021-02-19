6430c8309b080fa5c93d212b04a55d278f18b54a
Nataliia Pyvovartseva <raxma@ukr.net>
2021-02-19 11:54:52 +0200

    Add material-ui CssBaseline

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducers from './reducers';
 import middleware from './middleware';
+import CssBaseline from "@material-ui/core/CssBaseline";
 import './index.css';
 import App from './components/App';
 
@@ -16,6 +16,7 @@ const history = createBrowserHistory();
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
+      <CssBaseline />
       <App />
     </Router>
   </Provider>,
